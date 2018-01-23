4e9dbc2178a58fac1a13bef8d9a71568a982a9e9
Mauricio Rivera <mauriveravela@gmail.com>
2018-01-22 18:39:12 -0600

    Commit Numero 1

---
+++ src/com/prueba/java/main/principal.java
@@ -0,0 +1,10 @@
+package com.prueba.java.main;
+
+public class principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Que show muchachones, probando el si jala el Java 8");
+	}
+
+}
