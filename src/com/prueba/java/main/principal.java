ddd6f5b36d982a04c8f2d2de1b59743feb76c723
Mauricio Rivera <mauriveravela@gmail.com>
2018-01-22 18:48:37 -0600

    Commit número 2, se corrige la redacción

--- src/com/prueba/java/main/principal.java
+++ src/com/prueba/java/main/principal.java
@@ -3,8 +3,8 @@ package com.prueba.java.main;
 public class principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			System.out.println("Que show muchachones, probando el si jala el Java 8");
+			System.out.println("Que show muchachones, probando si jala el Java 8");
+			
 	}
 
 }
